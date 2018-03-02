ee62144dff6891a8deb7195f3b120a16f7ea3803
Florian Stöcker <florian.stoecker@gmx.net>
2018-03-02 14:20:06 +0100

    update OoP

--- src/main/java/com/innoq/praktikum/viergewinnt/KIGegnerStaerkeDrei.java
+++ src/main/java/com/innoq/praktikum/viergewinnt/KIGegnerStaerkeDrei.java
@@ -3,46 +3,51 @@ package com.innoq.praktikum.viergewinnt;
 public class KIGegnerStaerkeDrei extends KIGegner {
     public void macheZug(Konsole oberflaeche, Spielfeld spielfeld)
     {
+        int anDerReihe = spielfeld.getAnDerReihe();
+        spielfeld.setAnDerReihe(1);
         if(kannGewinnen(spielfeld) == true)
         {
+            spielfeld.wechseln();
             steinWurdeAusgewaehlt(spielfeld, oberflaeche);
             return;
         }
-
+        spielfeld.setAnDerReihe(2);
         if(kannGewinnen(spielfeld) == true)
         {
-
             steinWurdeAusgewaehlt(spielfeld, oberflaeche);
             return;
         }
         spielfeld.wechseln();
         if(zweiGleicheGewinnMöglich(0, spielfeld) == true)
         {
+            spielfeld.wechseln();
             steinWurdeAusgewaehlt(spielfeld, oberflaeche);
             return;
         }
+        spielfeld.setAnDerReihe(2);
         if(zweiGleicheGewinnMöglich(0, spielfeld) == true)
         {
             steinWurdeAusgewaehlt(spielfeld, oberflaeche);
             return;
         }
+        spielfeld.setAnDerReihe(anDerReihe);
+            if(eigenerStein(spielfeld) == true)
+            {
+                steinWurdeAusgewaehlt(spielfeld, oberflaeche);
+                return;
+            }
 
-                if(eigenerStein(spielfeld) == true)
-                {
-                    steinWurdeAusgewaehlt(spielfeld, oberflaeche);
-                }
-
-                else
+            else
+            {
+                random(spielfeld);
+                while(spielfeld.legalerZug(oberflaeche) == false)
                 {
                     random(spielfeld);
-                    while(spielfeld.legalerZug(oberflaeche) == false)
-                    {
-                        random(spielfeld);
-                    }
-                    int auswahlSpalte = spielfeld.getInsertPos();
-                    spielfeld.wirfSteinEin(oberflaeche,spielfeld);
-                    oberflaeche.kiGelegtText(auswahlSpalte + 1);
                 }
+                int auswahlSpalte = spielfeld.getInsertPos();
+                spielfeld.wirfSteinEin(oberflaeche,spielfeld);
+                oberflaeche.kiGelegtText(auswahlSpalte + 1);
+            }
 
 
     }
